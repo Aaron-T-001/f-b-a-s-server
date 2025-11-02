5fe592da1f26ccfc9bf0660dcaad522c3cd7669b
Aaron-T-001 <aarontoyosi@gmail.com>
2025-11-02 23:43:24 +0800

    Update wss_server.js

--- wss_server.js
+++ wss_server.js
@@ -31,10 +31,10 @@ const PORT = 443;
 
 const options = {
   key: fs.readFileSync(
-    "/etc/letsencrypt/live/api.smartattendancesystem.com.ng/privkey.pem"
+    "/etc/letsencrypt/live/api.engineeringattendancesystem.com.ng/privkey.pem"
   ),
   cert: fs.readFileSync(
-    "/etc/letsencrypt/live/api.smartattendancesystem.com.ng/fullchain.pem"
+    " /etc/letsencrypt/live/api.engineeringattendancesystem.com.ng/fullchain.pem"
   ),
 };
 
